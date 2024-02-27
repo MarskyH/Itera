dab6b283c0f74844ccc701ac0a96edcd463c17dd
Marcus Huriel Lira Loureiro <marcus.loureiro@basa.com.br>
2024-02-27 08:55:20 -0300

    fix: oruga e customInput2

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CadastroView from '../views/CadastroView.vue'
 import LoginView from '../views/LoginView.vue'
-import HomeView from '@/views/HomeView.vue'
+import HomeView from 'src/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import paths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), paths()],
   resolve: {
+    mainFields: ["module"],
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      src: path.resolve(__dirname, 'src/'),
+      components: path.resolve(__dirname, 'src/components/'),
+      pages: path.resolve(__dirname, 'src/pages/')
     }
-  }
+  },
 })
-
-
