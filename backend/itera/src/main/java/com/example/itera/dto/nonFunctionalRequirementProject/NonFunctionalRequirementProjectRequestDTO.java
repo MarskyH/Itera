0b8fb5be8709e50512c0acb7fa5ca5f26420300f
Marcus Loureiro <marcushuriel08@hotmail.com>
2024-04-16 15:18:11 -0300

    refactor: rota put nonFunctionalRequirementProject

--- backend/itera/src/main/java/com/example/itera/dto/nonFunctionalRequirementProject/NonFunctionalRequirementProjectRequestDTO.java
+++ backend/itera/src/main/java/com/example/itera/dto/nonFunctionalRequirementProject/NonFunctionalRequirementProjectRequestDTO.java
@@ -1,7 +1,7 @@
 package com.example.itera.dto.nonFunctionalRequirementProject;
 
 
-public record NonFunctionalRequirementProjectRequestDTO(String project_id, String nonfunctionalrequirement_id, Integer weight) {
+public record NonFunctionalRequirementProjectRequestDTO(String id, String project_id, String nonfunctionalrequirement_id, Integer weight) {
 
 }
 
