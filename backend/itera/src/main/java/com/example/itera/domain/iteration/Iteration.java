e09f65f30dbadfea7a2c3becdb0c2b7f4f1f7f83
Marcus Huriel Lira Loureiro <marcus.loureiro@basa.com.br>
2024-04-17 09:02:07 -0300

    feat: iteration domain

---
+++ backend/itera/src/main/java/com/example/itera/domain/iteration/Iteration.java
@@ -0,0 +1,33 @@
+package com.example.itera.domain.iteration;
+
+import com.example.itera.domain.project.Project;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "iteration")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Iteration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer number;
+    private Timestamp startDate;
+    private Timestamp endDate;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "project_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Project project;
+
+}
