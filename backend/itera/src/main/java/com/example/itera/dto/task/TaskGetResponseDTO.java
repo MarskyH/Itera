fdda56225786f8a219e619a46b4ae69a37d90549
Marcus Loureiro <marcushuriel08@hotmail.com>
2024-06-16 17:46:26 -0300

    feat: get task por id com detalhes

---
+++ backend/itera/src/main/java/com/example/itera/dto/task/TaskGetResponseDTO.java
@@ -0,0 +1,40 @@
+package com.example.itera.dto.task;
+
+import com.example.itera.domain.task.Task;
+import com.example.itera.dto.taskRequirement.TaskRequirementResponseDTO;
+import com.example.itera.dto.taskImprovement.TaskImprovementResponseDTO;
+import com.example.itera.dto.taskBug.TaskBugResponseDTO;
+
+import java.sql.Timestamp;
+
+public record TaskGetResponseDTO(
+        String id,
+        String title,
+        String priority,
+        Timestamp startDate,
+        Timestamp endDate,
+        Integer orderTask,
+        String listName,
+        String taskType,
+        TaskRequirementResponseDTO taskRequirement,
+        TaskImprovementResponseDTO taskImprovement,
+        TaskBugResponseDTO taskBug,
+        String iteration_id
+) {
+    public TaskGetResponseDTO(Task task) {
+        this(
+                task.getId() != null ? task.getId() : "",
+                task.getTitle(),
+                task.getPriority(),
+                task.getStartDate(),
+                task.getEndDate(),
+                task.getOrderTask(),
+                task.getListName(),
+                task.getTaskType(),
+                task.getTaskRequirement() != null ? new TaskRequirementResponseDTO(task.getTaskRequirement()) : null,
+                task.getTaskImprovement() != null ? new TaskImprovementResponseDTO(task.getTaskImprovement()) : null,
+                task.getTaskBug() != null ? new TaskBugResponseDTO(task.getTaskBug()) : null,
+                task.getIteration() != null && task.getIteration().getId() != null ? task.getIteration().getId() : ""
+        );
+    }
+}
